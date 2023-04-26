a385f66d2b83bbd25eade5f141e610b21c67138c
Asil <moonbizhi@gmail.com>
2023-04-27 00:12:33 +0300

    successfully train with new model

--- socio_epi_networks/epifit2.py
+++ socio_epi_networks/epifit2.py
@@ -25,13 +25,9 @@ def get_ER_random_contact(n, avgDegree):
     contact = torch.FloatTensor(graph).to(device)
     return contact
 
-_ps= torch.tensor([ [0, 0.2, 0, 0],
-                    [0, 0, 0.2, 0],
-                    [0, 0, 0, 0.2],
-                    [0.2, 0, 0, 0]], device=device)
 
 class EPI_dense(torch.nn.Module):
-    def __init__(self, ISNet, psMatrix, population, device):
+    def __init__(self, ISNet, psMatrix, population, device, train=False):
         super(EPI_dense, self).__init__()
         self._n= ISNet.size()[0]
         self._IS= ISNet
@@ -39,8 +35,10 @@ class EPI_dense(torch.nn.Module):
         self._II= self._EE
         self._RR= self._EE
         self._state= torch.stack((population["S"], population["E"], population["I"], population["R"]))
-        self._L= torch.zeros_like(self._state, device=device)
         self._psMatrix= psMatrix
+        self._train= train
+        if train==True:
+            self._psMatrix=torch.nn.Parameter(psMatrix)
         self._P= self._state
     
     def reset_population(self, population):
@@ -48,17 +46,25 @@ class EPI_dense(torch.nn.Module):
         self._P= self._state
 
     def forward(self):
-        self._L[0]= self._state[0]*torch.matmul(self._IS, self._state[2])
-        self._L[1]= torch.matmul(self._state[1], self._EE)
-        self._L[2]= torch.matmul(self._state[2], self._II)
-        self._L[3]= torch.matmul(self._state[3], self._RR)
+        stateGradient= torch.clone(self._state)
+        L= torch.zeros_like(self._state, device=device)
+        L[0]= stateGradient[0]*torch.matmul(self._IS, stateGradient[2])
+        L[1]= torch.matmul(stateGradient[1], self._EE)
+        L[2]= torch.matmul(stateGradient[2], self._II)
+        L[3]= torch.matmul(stateGradient[3], self._RR)
+        if self._train==False:
+            prob= 1- torch.exp(torch.matmul(torch.log(1 - self._psMatrix.T), L))
+        else:
+            psMatrix= torch.sigmoid(self._psMatrix)
+            prob= 1- torch.exp(torch.matmul(torch.log(1 - psMatrix.T), L))
+        _stable_prob= 1- torch.sum(prob, 0)
+        prob= prob+ stateGradient*_stable_prob
 
-        self._P= 1- torch.exp(torch.matmul(torch.log(1 - self._psMatrix.T), self._L))
-        _stable_prob= 1- torch.sum(self._P, 0)
-        self._P= self._P+ self._state*_stable_prob
-        self._state= self.rample_uniform_matrix(self._P)
+        with torch.no_grad():
+            self._state= self.rample_uniform_matrix(prob)
+            self._P= prob
 
-        return self._state, self._P
+        return torch.sum(self._state, 1), torch.sum(prob, 1)
 
     #sample nxm pobability matrix, of 0 dimension, which contains n choise for a random variable
     def rample_uniform_matrix(self, P):  
@@ -70,10 +76,6 @@ class EPI_dense(torch.nn.Module):
             U= U+state[i]
         return state
     
-    def get_population_number(self):
-
-        return torch.sum(self._state, 1)
-    
     def get_population(self):
         return { #a columnar DB somewhat reminds the Pandas DataFrame 
                 "S": self._state[0],
@@ -81,9 +83,35 @@ class EPI_dense(torch.nn.Module):
                 "I": self._state[2],
                 "R": self._state[3],
                 }
-    def get_expectation(self):
-        return torch.sum(self._P, 1)
+    def get_population(self):
+
+        return self._state
+    
+    def get_probability(self):
+        return self._P
     
     def get_probability_transition_matrix(self):
         
         return self._psMatrix
+    
+    def get_psMatrix(self):
+
+        return torch.sigmoid(self._psMatrix)
+
+
+if __name__=="__main__":
+    n= 1000
+    avgDegree= 5
+    timeHorizon= 20
+    contact=get_ER_random_contact(n, avgDegree)
+    contact = contact.requires_grad_(True)
+    realData =None
+
+    with torch.no_grad():
+        ps= torch.tensor([[0, 0.2, 0, 0],
+                        [0, 0, 0.99, 0],
+                        [0, 0, 0, 0.0],
+                        [0.0, 0, 0, 0]], device=device)
+        population= population(n, device)
+        model= EPI_dense(contact, ps, population, device)
+    pass
